0ad9a63a11aa40d4500a5af5fb5fcaf16db07424
Agilist Developer <dev@agilistgroup.com>
2019-10-16 13:40:58 +0530

    Database added

--- createTables.py
+++ createTables.py
@@ -1,3 +1,18 @@
-import sqllite3
+import sqlite3
 
-conn = sqllite3.connect('database.db')
+conn = sqlite3.connect('database.db')
+print("Database created successfully!!")
+
+conn.execute('''CREATE TABLE customers (
+	custID INT PRIMARY KEY,
+	custName TEXT,
+	custAddress TEXT,
+	custCity TEXT,
+	custState TEXT,
+	custPin TEXT,
+	custEmail TEXT,
+	custMobile TEXT,
+	country TEXT
+	)''')
+print("Table created successfully!!")
+conn.close()

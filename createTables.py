fa632a07e2ee4bc869e0a560b4780a1e5b751b04
Muskaan-13 <muskaannmalik@gmail.com>
2019-10-16 15:00:33 -0700

    Database Updated

---
+++ createTables.py
@@ -0,0 +1,57 @@
+import sqlite3
+
+conn = sqlite3.connect('database.db')
+print("Database created successfully!!")
+
+conn.execute('''CREATE TABLE IF NOT EXISTS customers (
+	custID INT PRIMARY KEY,
+	custName TEXT,
+	custAddress TEXT,
+	custCity TEXT,
+	custState TEXT,
+	custPin TEXT,
+	custEmail TEXT,
+	custPassword TEXT,
+	custMobile TEXT,
+	country TEXT
+	)''')
+
+conn.execute('''CREATE TABLE IF NOT EXISTS company(
+	compID INT PRIMARY KEY,
+	compName TEXT,
+	compAddress TEXT,
+	compCity TEXT,
+	compState TEXT,
+	compPin TEXT,
+	compEmail TEXT,
+	compPassword TEXT,
+	compMobile TEXT,
+	compCode TEXT,
+	compCountry TEXT
+	)''')
+conn.execute('''CREATE TABLE IF NOT EXISTS products(
+	proID INT PRIMARY KEY,
+	proName TEXT,
+	proQuantity INT,
+	proPrice REAL,
+	proBrandID INT ,
+	proDescription TEXT,
+	proImage TEXT,
+	FOREIGN KEY(proBrandID) REFERENCES company(compID)
+	)''')
+conn.execute('''CREATE TABLE IF NOT EXISTS wishlist(
+	wishUserID INT,
+	wishProductID INT,
+	FOREIGN KEY(wishUserID) REFERENCES customer(custID),
+	FOREIGN KEY(wishProductID) REFERENCES company(compID)
+	)''')
+conn.execute('''CREATE TABLE IF NOT EXISTS cart(
+	cartUserID INT,
+	cartProductID INT,
+	FOREIGN KEY(cartUserID) REFERENCES customer(custID),
+	FOREIGN KEY(cartProductID) REFERENCES company(compID)
+	)''')
+
+
+print("Table created successfully!!")
+conn.close()
